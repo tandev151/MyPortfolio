cd8a3ba7bc1e1fa31621908df262123d6d7fdaea
Tan IT <tanleit151@gmail.com>
2025-06-01 10:14:36 +0700

    update portfolio

--- script.js
+++ script.js
@@ -2,48 +2,48 @@ $(document).ready(function () {
   $(window).scroll(function () {
     // sticky navbar on scroll script
     if (this.scrollY > 20) {
-      $(".navbar").addClass("sticky");
+      $('.navbar').addClass('sticky');
     } else {
-      $(".navbar").removeClass("sticky");
+      $('.navbar').removeClass('sticky');
     }
 
     // scroll-up button show/hide script
     if (this.scrollY > 500) {
-      $(".scroll-up-btn").addClass("show");
+      $('.scroll-up-btn').addClass('show');
     } else {
-      $(".scroll-up-btn").removeClass("show");
+      $('.scroll-up-btn').removeClass('show');
     }
   });
   // slide-up script
-  $(".scroll-up-btn").click(function () {
-    $("html").animate({ scrollTop: 0 });
+  $('.scroll-up-btn').click(function () {
+    $('html').animate({ scrollTop: 0 });
     // removing smooth scroll on slide-up button click
-    $("html").css("scrollBehavior", "auto");
+    $('html').css('scrollBehavior', 'auto');
   });
-  $(".navbar .menu li a").click(function () {
+  $('.navbar .menu li a').click(function () {
     // applying again smooth scroll on menu items click
-    $("html").css("scrollBehavior", "smooth");
+    $('html').css('scrollBehavior', 'smooth');
   });
   // toggle menu/navbar script
-  $(".menu-btn").click(function () {
-    $(".navbar .menu").toggleClass("active");
-    $(".menu-btn i").toggleClass("active");
+  $('.menu-btn').click(function () {
+    $('.navbar .menu').toggleClass('active');
+    $('.menu-btn i').toggleClass('active');
   });
   // typing text animation script
-  var typed = new Typed(".typing", {
-    strings: ["Developer"],
+  var typed = new Typed('.typing', {
+    strings: ['Software Engineer'],
     typeSpeed: 100,
     backSpeed: 60,
-    loop: true,
+    loop: true
   });
-  var typed = new Typed(".typing-2", {
-    strings: ["Developer"],
+  var typed = new Typed('.typing-2', {
+    strings: ['Software Engineer'],
     typeSpeed: 100,
     backSpeed: 60,
-    loop: true,
+    loop: true
   });
   // owl carousel script
-  $(".carousel").owlCarousel({
+  $('.carousel').owlCarousel({
     margin: 20,
     loop: true,
     autoplay: true,
@@ -52,16 +52,16 @@ $(document).ready(function () {
     responsive: {
       0: {
         items: 1,
-        nav: false,
+        nav: false
       },
       600: {
         items: 2,
-        nav: false,
+        nav: false
       },
       1000: {
         items: 3,
-        nav: false,
-      },
-    },
+        nav: false
+      }
+    }
   });
 });
